27a7532b5041c0c68e1eee07a257b8b0e222cd02
Violetta <violka.kovalenko@gmail.com>
2024-03-13 23:41:20 +0200

    first task

---
+++ PZ/ConsoleApp1/ConsoleApp1/Element.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Element
+    {
+        public string Appearence { get; set; } = "default element";
+        public string Name { get; set; } = "Default name";
+    }
+}
---
+++ PZ/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Element element = new Element();
+            element.Appearence = "&";
+            element.Name = "Ampersand";
+
+            Console.Write(element.Appearence);
+            Console.WriteLine(element.Name);
+
+            Element[,] matrix = new Element[10, 10];
+
+            //заповнюємо матрицю
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = new Element();
+                    matrix[i, j].Appearence = "&";
+                    matrix[i, j].Name = "Ampersand";
+                }
+            }
+            //Виводимо на консоль елементи матриці
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine(matrix[i, j].Appearence);
+                    Console.WriteLine(matrix[i, j].Name);
+                }
+            }
+
+
+
+        }
+    }
+}
